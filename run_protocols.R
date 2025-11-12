185f0529a03bfae4db6ca2999084313b35c3f686
jshinb <shin.jihyung@gmail.com>
2025-11-12 17:04:20 -0500

    Modify to accommodate family-based data

--- run_protocols.R
+++ run_protocols.R
@@ -1,8 +1,10 @@
 #*****************************************************************************#
-# Load inputs and set working directories ----
+# Load inputs and set working directories: CHANGE HERE ----
+#* [Put the full path of the input file]
+cohort_input_file =
+#
 #*****************************************************************************#
 
-cohort_input_file = #full path "~/Library/CloudStorage/OneDrive-Personal/neuroCHARGE_obesity_ctxVol_lipids/distribute/cohort_specific_inputs_testing_hypothetical.R"
 source(cohort_input_file)
 setwd(wd)
 
@@ -16,6 +18,9 @@ opt = data.frame(
 )# required:
 
 outdir0=file.path(wd,paste0("outputs_to_send_",opt$cohort_name))
+if(opt$is.family.data){
+  outdir0 = paste0(outdir0,"_FAM") 
+}
 dir.create(outdir0)
 file.copy(cohort_input_file ,outdir0,overwrite = T)
 
@@ -30,45 +35,53 @@ source("1_Pheno_preparation.R")
 pooled_analysis(data=df_all %>% filter(is.underweight=="No",age.group=="(34,65]_(34,65]"),
                 cov_cohort_specific = cov_cohort_specific, 
                 cov_additional = cov_additional, 
-                analysis.type = "primary")
+                analysis.type = "primary",
+                is.family.data = opt$is.family.data)
 
 # Secondary analyses
 #* [APOE4-stratified analyses of non-underweight individuals at midlife]
 str_analysis(data = df_all%>% filter(is.underweight=="No",age.group=="(34,65]_(34,65]"), 
              str.var = "APOE4",str.var.name =  "APOE4.present",
              cov_cohort_specific=cov_cohort_specific,
-             cov_additional = cov_additional)
+             cov_additional = cov_additional,
+             is.family.data = opt$is.family.data)
 
 #* [Age-stratified analyses of non-underweight individuals]
 str_analysis(data=df_all %>% filter(is.underweight=="No"), 
              str.var = "Age",str.var.name =  "age.group",
              cov_cohort_specific=cov_cohort_specific,
-             cov_additional = cov_additional)
+             cov_additional = cov_additional,
+             is.family.data = opt$is.family.data)
 
 #* [BMI-stratified analyses of individuals at midlife]
 str_analysis(data = df_all%>% filter(age.group=="(34,65]_(34,65]"), 
              str.var = "BMI",str.var.name = "is.underweight",
              cov_cohort_specific=cov_cohort_specific,
-             cov_additional = cov_additional)
+             cov_additional = cov_additional,
+             is.family.data = opt$is.family.data)
 
 #* [Ethnicity-stratified analyses of non-underweight individuals at midlife] # 
 str_analysis(data = df_all%>% filter(is.underweight=="No",age.group=="(34,65]_(34,65]"), 
              str.var = "Ethnicity",str.var.name = "ethnicity",
              cov_cohort_specific=cov_cohort_specific,
-             cov_additional = cov_additional)
+             cov_additional = cov_additional,
+             is.family.data = opt$is.family.data)
 
 #* [Ancestry-stratified analyses of non-underweight individuals at midlife] # 
 str_analysis(data = df_all%>% filter(is.underweight=="No",age.group=="(34,65]_(34,65]"), 
              str.var = "Ancestry",str.var.name = "ancestry",
              cov_cohort_specific=cov_cohort_specific,
-             cov_additional = cov_additional)
+             cov_additional = cov_additional,
+             is.family.data = opt$is.family.data)
 
 #* [Pooled analyses of non-underweight individuals at midlife adjusting for ethnicity] # 
 pooled_analysis(data = df_all%>% filter(is.underweight=="No",age.group=="(34,65]_(34,65]"),
                 cov_cohort_specific = c(cov_cohort_specific,"ethnicity"), 
-                cov_additional = cov_additional,analysis.type = "adjEthnicity")
+                cov_additional = cov_additional,analysis.type = "adjEthnicity",
+                is.family.data = opt$is.family.data)
 
 #* [Pooled analyses of all individuals]
 pooled_analysis(data=df_all,cov_cohort_specific = cov_cohort_specific, 
                 cov_additional = cov_additional, 
-                analysis.type = "primary_all")
+                analysis.type = "primary_all",
+                is.family.data = opt$is.family.data)
