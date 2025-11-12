f0ae183ff2911bb195dc977814ffcb8f06ae03f5
jshinb <shin.jihyung@gmail.com>
2025-11-12 13:12:35 -0500

    Edit example

--- cohort_specific_inputs.R
+++ cohort_specific_inputs.R
@@ -2,23 +2,24 @@
 # Analyst/PI information for contact and study information: put double or single quotation
 # marks
 #
-analysis_date =     # YYYY-MM-DD
+analysis_date = #*[YYYY-MM-DD]
 analyst = 
 analyst_email = 
-PI =  #use c('PI1 name','PI2 name',...) if more than 1 
-PI_email = #use c('PI1 email','PI2 email',...) if more than 1 
-cohort_full_name = #'Saguenay Youth Study' 
-cohort_short_name = #'SYS_parents' 
-ethnicities = # or c('White', 'African American') 
-ancestries = # (AFR, AMR (American-Hispanic?), EAS, EUR, SAS)
-FreeSurfer_version = #e.g., '5.3.0'
-ICV_based_on =  #e.g., FreeSurfer/ customized pipeline (ref:PMID)
-ICV_pipeline_reference = #'PMIDXXX'
+PI = #*[use c('PI1 name','PI2 name',...) if more than 1]
+PI_email = #*[use c('PI1 email','PI2 email',...) if more than 1]
+cohort_full_name = #*[e.g., 'Saguenay Youth Study']
+cohort_short_name = #*[e.g., 'SYS_parents']
+ethnicities = #*["White" or use c('White', 'African American') in the presence of multiple ethnicities]
+ancestries = #*["EUR" or c('AFR', 'AMR', 'EAS', 'EUR', 'SAS',...) in the presence of multiple ancestries]
+FreeSurfer_version = #*[e.g., '5.3.0']
+ICV_based_on =  #*[e.g., FreeSurfer/ customized pipeline (ref:PMID)]
+ICV_pipeline_reference = #*['PMIDXXX']
 
 # DATA INFORMATION --------------------------------------------------------------------- #
 # working directory(wd), file names for the brain and non-brain datasets, and missing value code
-wd=#directory where you downloaded the R scripts
-input_neuro=#path to the data file with brain image variables "../data/neuro_SPS.txt"
-input_non.neuro=# path to the data file with non-brain variables'../data/non.neuro_hypothetical_SPS.txt'
-missing_value_code='NA' #how missing values are coded
+wd = #*[directory where the R scripts and data files are stored]
+input_neuro = #*[name of data file with brain image variables: e.g., "neuro_SPS.txt"]
+input_non.neuro = #*[name of the data file with non-brain variables: e.g., 'non.neuro_SPS.txt']
+missing_value_code ='NA' #*[how missing values are coded]
 #----------------------------------------------------------------------------------------#
+
